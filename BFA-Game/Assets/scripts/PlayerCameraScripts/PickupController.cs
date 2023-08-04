ff0847c95b755a9d067f181cdf89a506b3eeb6fb
Thetruereed <arreed@student.cia.edu>
2023-08-04 15:24:58 -0400

    added grabbing objects

---
+++ BFA-Game/Assets/scripts/PlayerCameraScripts/PickupController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    [Header("Pickup Settings")]
+    [SerializeField] Transform holdArea;
+    private GameObject heldobj;
+    private Rigidbody heldobjRB;
+
+
+    [Header("Physics Parameters")]
+    [SerializeField] private float pickupRange = 5.0f;
+    [SerializeField] private float pickupForce = 150.0f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (heldobj == null)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRange))
+                {
+                    PickupObject(hit.transform.gameObject);
+                }
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+        if (heldobj != null)
+        {
+            MoveObject();
+        }
+    }
+
+    void MoveObject()
+    {
+        if (Vector3.Distance(heldobj.transform.position, holdArea.position) > 0.1f)
+        {
+            Vector3 moveDirection = (holdArea.position - heldobj.transform.position);
+            heldobjRB.AddForce(moveDirection * pickupForce);
+        }
+    }
+
+    void PickupObject(GameObject pickobj)
+    {
+        if (pickobj.GetComponent<Rigidbody>())
+        {
+            heldobjRB = pickobj.GetComponent<Rigidbody>();
+            //turns off gravity for objects so that object does not fall when picked up
+            heldobjRB.useGravity = false;
+            heldobjRB.drag = 10f;
+            // freeze rotation of object when picked up
+            heldobjRB.constraints = RigidbodyConstraints.FreezeRotation;
+
+            heldobjRB.transform.parent = holdArea;
+            heldobj = pickobj;
+        }
+    }
+
+    void DropObject()
+    {
+        //turns on gravity for objects so that object falls when dropped
+        heldobjRB.useGravity = true;
+        heldobjRB.drag = 1f;
+        // unfreeze rotation of object when dropped
+        heldobjRB.constraints = RigidbodyConstraints.None;
+
+        heldobj.transform.parent = null;
+        heldobj = null;
+    }
+}
